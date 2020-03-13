7f299b2ce92f12649c1602b92936ead1bf5c4150
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-03-13 16:22:11 -0400

    help menu works, rollcall ping works

--- rollcall_bot.py
+++ rollcall_bot.py
@@ -1,25 +1,36 @@
 import os
 
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='howto')
+async def list_help(ctx):
+
+    response = 'Here is a list of commands you can use me for!\n' \
+                '**!rollcall** - Starts a rollcall for Siege! Defaults to ' \
+                '8PM\n' \
+                '**!in** - If there is a rollcall, use this to show you\'re' \
+                ' in for Siege tonight\n' \
+                '**!out** - If you changed your mind and are now out\n' \
+                '**!whosin** - Gives you a list of people who are in for Siege'\
+                ''
+    await ctx.send(response)
 
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to the Discord!')
+@bot.command(name='rollcall', pass_context=True)
+async def start_rollcall(ctx):
+    sender = str(ctx.message.author.mention)
+    await ctx.send(f'@here {sender} has initiated a Siege Roll Call for 8PM!')
+    await send_whos_in(ctx)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
 
-client.run(TOKEN)
+async def send_whos_in(ctx):
+    await ctx.send('Lets try this')
+
+bot.run(TOKEN)
